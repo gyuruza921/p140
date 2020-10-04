1d2df32f8d29888549265c0238fc9ebe274e6938
gyurza921 <objekt.172.uru@gmail.com>
2020-10-04 18:02:44 +0900

    add function machin_hand() referee()

--- main.py
+++ main.py
@@ -12,6 +12,8 @@
 # 前準備
 # 
 
+import random
+
 # handクラスの作成
 class Hand():
     def __init__(self, name, hand):
@@ -19,19 +21,56 @@ class Hand():
         self.hand = hand
 
 # 処理部
-# ランダムに出す手を決める
-# プレイヤーとマシンの出した手を比較
+# ランダムにマシンの出す手を決める
+def machin_hand():
+    # result = Hand("CPU", random.randint(0, 2))
+    return Hand("CPU", random.randint(0, 2))
 
-# 表示部
+# プレイヤーとマシンの出した手を比較
+def referee(player1, player2):
+    # 条件分岐
+    # 引き分け
+    if player1.hand == player2.hand:
+        print("引き分け")
+    # player1がグーを出した場合
+    elif player1.hand == 0:
+        # player2がチョキを出した場合
+        if player2.hand == 1:
+            print(f"{player1.name:s}の勝ちです")
+        # player2がパーを出した場合
+        else:
+            print(f"{player2.name:s}の勝ちです")
+
+    # player1がチョキを出した場合
+    elif player1.hand == 1:
+        # player2がパーを出した場合
+        if player2.hand == 2:
+            print(f"{player1.name:s}の勝ちです")
+        # player2がグーを出した場合
+        else:
+            print(f"{player2.name:s}の勝ちです")
+
+    # player1がパーを出した場合
+    elif player1.hand == 2:
+        # player2がグーを出した場合
+        if player2.hand == 0:
+            print(f"{player1.name:s}の勝ちです")
+        # player2がチョキを出した場合
+        else:
+            print(f"{player2.name:s}の勝ちです")
+
+
 # 対戦結果を表示する
 
 # 操作部
-player_name = input("名前を入力してください")
+# プレイヤーの名前と出す手を入力
+player_name = input("名前を入力してください ")
 player_hand = input("選んでくださいグー:0 チョキ:1 パー:2 ")
 player_hand = int(player_hand)
-
 player = Hand(player_name, player_hand)
 
-print(player)
-print(player.name)
-print(player.hand)
+# マシンの出す手を選択
+machin = machin_hand()
+
+# 判定と結果の表示
+referee(player, machin)
